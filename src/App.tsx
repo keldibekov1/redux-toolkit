74cc2ffa899247907de46c6a68a26d848d1660f2
O'tkir <keldibekovotkir767@gmail.com>
2025-05-28 05:48:49 +0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { lazy } from "react";
-import { Link, useRoutes } from "react-router-dom";
+import {  useRoutes } from "react-router-dom";
 // import Home from './pages/Home'
 // import About from './pages/About'
-const Home = lazy(() => import("./pages/Home"));
+// const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 const Comment = lazy(() => import("./pages/comments/Comment"));
 const Country = lazy(() => import("./pages/Country/Country"));
@@ -10,15 +10,13 @@ const Country = lazy(() => import("./pages/Country/Country"));
 const App = () => {
   return (
     <>
-    <Link to={"/"}>Home</Link>
-    <Link to={"/about"}>About</Link>
-    <Link to={"/comment"}>Comment</Link>
-    <Link to={"/country"}>Country</Link>
+     
+
       {useRoutes([
-        { path: "/", element: <Home /> },
+        { path: "/", element: <Country /> },
         { path: "/about", element: <About /> },
         { path: "/comment", element: <Comment /> },
-            { path: "/country", element: <Country /> },
+        { path: "/country", element: <Country /> },
       ])}
     </>
   );
